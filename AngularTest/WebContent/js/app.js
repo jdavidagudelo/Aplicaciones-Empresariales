f77f192806a9005d3f08a79929d8cd8c04217c1c
jdavidagudelo <jdaaa2009@gmail.com>
2014-06-23 10:47:25 -0500

    Directivas.

--- AngularTest/WebContent/js/app.js
+++ AngularTest/WebContent/js/app.js
@@ -1,35 +1,69 @@
 /**
  * 
  */
-(function(){
+(function() {
 	var app = angular.module('store', []);
-	app.controller("StoreController",function(){
+	app.controller("StoreController", function() {
 		this.products = phones;
 	});
-	app.controller("PanelController", function(){
-		this.tab=1;
-		this.selectTab = function(setTab)
-		{
+	app.controller("PanelController", function() {
+		this.tab = 1;
+		this.selectTab = function(setTab) {
 			this.tab = setTab;
 		};
-		this.isSelected = function(checkTab){
-			return this.tab===checkTab;
+		this.isSelected = function(checkTab) {
+			return this.tab === checkTab;
 		};
 	});
-	var phones = [{
-			name:'Motorola',
-			price: 7.95,
-			description : 'Figura',
-			canPurchase : true,
-			soldOut : false,
-			images : ["img/phones/dell-streak-7.0.jpg","img/phones/dell-venue.3.jpg"],
-			
-	},{
-		name:'Samsung',
-		price: 4.95,
-		description : 'Figura',
-		canPurchase : true,
-		soldOut : false,
-		images : ["img/phones/dell-streak-7.0.jpg"],
-},];
+	app.controller("ReviewController", function(){
+		this.review = {};
+		this.addReview = function(product)
+		{
+			this.review.createdOn = Date.now();
+			product.reviews.push(this.review);
+			this.review = {};
+		};
+	});
+	app.directive('productTitle', function(){
+		return {
+			 restrict: 'E',
+			templateUrl: "product-title.html"
+		};
+	});
+	var phones = [
+			{
+				name : 'Motorola',
+				price : 7.95,
+				description : 'Figura',
+				canPurchase : true,
+				soldOut : false,
+				images : [ "img/phones/dell-streak-7.0.jpg",
+						"img/phones/dell-venue.3.jpg" ],
+				reviews : [ {
+					stars : 5,
+					body : "buen producto",
+					author : "productowner@gmail.com"
+				}, {
+					stars : 4,
+					body : "buen producto",
+					author : "productowner@gmail.com"
+				}, ],
+
+			}, {
+				name : 'Samsung',
+				price : 4.95,
+				description : 'Figura',
+				canPurchase : true,
+				soldOut : false,
+				images : [ "img/phones/dell-streak-7.0.jpg" ],
+				reviews : [ {
+					stars : 5,
+					body : "buen producto",
+					author : "productowner@gmail.com"
+				}, {
+					stars : 4,
+					body : "buen producto",
+					author : "productowner@gmail.com"
+				}, ],
+			}, ];
 })();
